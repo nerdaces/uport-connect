4047ef392137d7df8cb3f38e6ae313e472a415a9
nerdaces <yuyammiki@gmail.com>
2020-10-24 00:53:56 +0900

    add: func to create and verify vc

--- examples/integration-tutorial/index.js
+++ examples/integration-tutorial/index.js
@@ -4,13 +4,13 @@
 var web3;
 const Connect = window.uportconnect
 const yourAppName = 'UportTutorial'
-const connect = new Connect(yourAppName, {network: 'rinkeby'})
+const connect = new Connect(yourAppName, { network: 'rinkeby' })
 const provider = connect.getProvider()
 web3 = new Web3(provider)
 // Work around to issue with web3 requiring a from parameter. This isn't actually used.
 web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
 
-const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
+const abi = [{ "constant": false, "inputs": [{ "name": "status", "type": "string" }], "name": "updateStatus", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "addr", "type": "address" }], "name": "getStatus", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }]
 const StatusContract = web3.eth.contract(abi).at('0x70A804cCE17149deB6030039798701a38667ca3B')
 
 const getAndRenderBlockchainState = (address) => {
@@ -23,7 +23,7 @@ const getAndRenderBlockchainState = (address) => {
   })
 }
 
-// If state already availabe in connect onload, render it
+// If state already available in connect onload, render it
 if (connect.did) {
   globalState.ethAddress = connect.address
   globalState.uportId = connect.did
@@ -62,4 +62,91 @@ const setStatus = () => {
     globalState.txHashSetStatus = txHash
     render()
   })
+}
+
+// Create VC for Setting Your Info
+const createVerifiedCredential = () => {
+  // error check for birthdayInput (ex. length, form)
+
+  const cred = {
+    claim: {
+      // name: globalState.nameInput,
+      birthday: globalState.birthdayInput
+    },
+    sub: globalState.uportId
+  }
+  connect.sendVerification(cred)
+}
+
+// Create VC for Adults
+const createVerifiedCredentialForAdult = () => {
+    // Get user's info
+    const req = {
+      requested: ['birthday'],
+      // verified: ['Adult'],
+      notification: true,
+    }
+    const reqID = 'disclosureReq'
+    connect.requestDisclosure(req, reqID)
+    connect.onResponse(reqID).then(cred => {
+      const yourAge = getAgeFromBirthday(cred.payload.birthday)
+      // Send verification
+      if (yourAge >= 20) {
+        const cred = {
+          claim: { adult: true },
+          sub: globalState.uportId
+        }
+        connect.sendVerification(cred)
+      } else {
+        console.log("Error: You are not an adult.")
+      }
+    })
+}
+
+// Verify your adultness
+const verifyYourAdultness = () => {
+  // Get user's info
+  const req = {
+    requested: ['adult'],
+    // verified: ['Adult'],
+    notification: true,
+  }
+  const reqID = 'disclosureReq'
+  connect.requestDisclosure(req, reqID)
+  connect.onResponse(reqID).then(cred => {
+    if (cred.payload.adult) {
+      globalState.flagAdult = "Yes!!!"
+    } else {
+      globalState.flagAdult = "No!!!"
+    }
+    $('#flagAdult').innerHTML = globalState.flagAdult; // fix later
+  })
+}
+
+const getAgeFromBirthday = (birthday) => {
+  if (!birthday) {
+    return -100
+  }
+  const yourBirthday = {
+    year: birthday.slice(0, 4),
+    month: birthday.slice(5, 7),
+    date: birthday.slice(8, 10),
+  }
+  // Dateインスタンスに変換
+  const birthDate = new Date(yourBirthday.year, yourBirthday.month - 1, yourBirthday.date);
+
+  // 文字列に分解
+  const y2 = birthDate.getFullYear().toString().padStart(4, '0');
+  const m2 = (birthDate.getMonth() + 1).toString().padStart(2, '0');
+  const d2 = birthDate.getDate().toString().padStart(2, '0');
+
+  // 今日の日付
+  const today = new Date();
+  const y1 = today.getFullYear().toString().padStart(4, '0');
+  const m1 = (today.getMonth() + 1).toString().padStart(2, '0');
+  const d1 = today.getDate().toString().padStart(2, '0');
+
+  // 引き算
+  const age = Math.floor((Number(y1 + m1 + d1) - Number(y2 + m2 + d2)) / 10000);
+  return age
 }
